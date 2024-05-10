ff0e167d74e058aaa7db2bda57ae822eb729b827
I3loodik <168632175+I3loodik@users.noreply.github.com>
2024-05-10 13:11:19 +0300

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-// Переключение между вкладками
-function openTab(tabName) {
-    var i;
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
- document.getElementById(tabName).style.display = "block";
-}
